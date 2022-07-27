0cdb50287326b4418c0160cce05c19fa5c22a886
Ajinkya Pawar <ajinkya.8080@gmail.com>
2022-07-27 17:06:57 +0530

    Added another Variable

--- src/test/java/testClassess/GitClass.java
+++ src/test/java/testClassess/GitClass.java
@@ -5,7 +5,8 @@ public class GitClass {
 	public static void main(String[] args) {
        int a = 4;
        int b = 3;
-       System.out.println(a+b);
+       int c = 2;
+       System.out.println(a+b+c);
 	}
 
 }
